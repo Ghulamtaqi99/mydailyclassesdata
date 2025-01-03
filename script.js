dab0502a7baeddf364da6da9d8f704611f19c4f5
Ghulamtaqi99 <123259124+Ghulamtaqi99@users.noreply.github.com>
2025-01-04 02:18:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const Name = ["Faizan", "Faiza", "Farhan", "Jani", "Janam", "jalwa"]
+Name.push("Farwa")
+Name.pop()
+Name.unshift("Farwa")
+Name.shift()
+
+console.log(Name)
+
+const Name1 = ["Faizan", "Faiza", "Farhan", "Jani", "Janam", "Better", "behaide",]
+
+const Name2 = Name1.splice(0, 4)
+
+console.log(Name1)
+
+
+
+// End This Class Start New Class
+
+
+const Fruit = ["apple", "mango", "orange", "banana", "apple",]
+console.log(Fruit.indexOf("banana"))
+
+
+const Num = [2,2,3,4,5,6,7,5]
+console.log(Num.lastIndexOf(5))
